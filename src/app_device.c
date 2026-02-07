31ff0e083b6d797a057bff1e86b96d0a24dfdd1c
Fedor <lfa1@yandex.ru>
2026-02-07 21:02:32 +0300

    Update app_device.c

--- src/app_device.c
+++ src/app_device.c
@@ -10,15 +10,15 @@ typedef struct {
 } config_switch_model_t;
 
 switch_device_t switch_device[DEVICE_SWITCH_MAX];
-device_switch_model_t device_switch_model = DEVICE_SWITCH_NONE;
+device_switch_model_t device_switch_model = DEVICE_MODEL;
 switch_device_t *device = &switch_device[DEVICE_SWITCH_NONE];
 
 static void device_gpio_init(device_gpio_t *device_gpio) {
+
     drv_gpio_func_set(device_gpio->gpio);
     drv_gpio_output_en(device_gpio->gpio, device_gpio->output);
     drv_gpio_input_en(device_gpio->gpio, device_gpio->input);
     if (device_gpio->pull) drv_gpio_up_down_resistor(device_gpio->gpio, device_gpio->pull);
-
 }
 
 static void device_model_init() {
@@ -28,8 +28,13 @@ static void device_model_init() {
     if (device_switch_model != DEVICE_SWITCH_NONE) {
         device_gpio_init(&device->button_gpio);
         device_gpio_init(&device->led_gpio);
-        device_gpio_init(&device->switch_gpio);
         device_gpio_init(&device->relay_gpio);
+        if (device_switch_model == DEVICE_SWITCH_2) {
+            drv_adc_init();
+            drv_adc_mode_pin_set(DRV_ADC_BASE_MODE, device->switch_gpio.gpio);
+            drv_gpio_up_down_resistor(device->switch_gpio.gpio, PM_PIN_PULLUP_10K);
+        } else
+            device_gpio_init(&device->switch_gpio);
 #if UART_PRINTF_MODE
         device_gpio_init(&device->debug_gpio);
         gpio_write(device->debug_gpio.gpio, 1);
@@ -51,7 +56,7 @@ void device_model_restore() {
     if (model_cfg.id == ID_DEVICE_SWITCH_CFG && model_cfg.crc == checksum((uint8_t*)&model_cfg, sizeof(config_switch_model_t)-1)) {
         device_switch_model = model_cfg.device_model;
     } else {
-        device_switch_model = DEVICE_SWITCH_NONE;
+        device_switch_model = DEVICE_MODEL;
     }
 
     device_model_init();
@@ -112,7 +117,7 @@ void device_init() {
 
     devi++;
 
-    /* zg301z Hobean */
+    /* zg301z Hobeian */
     switch_device[devi].device_en = ON;
     switch_device[devi].button_max = 1;
     switch_device[devi].button_gpio.gpio = GPIO_PB1;
@@ -130,8 +135,8 @@ void device_init() {
     switch_device[devi].switch_gpio.input = ON;
     switch_device[devi].switch_gpio.output = OFF;
     switch_device[devi].switch_gpio.func = AS_GPIO;
-    switch_device[devi].switch_gpio.pull = PM_PIN_PULLDOWN_100K;
-    switch_device[devi].switch_debounce = 128;
+    switch_device[devi].switch_gpio.pull = PM_PIN_PULLUP_10K;
+    switch_device[devi].switch_debounce = 16;
     switch_device[devi].relay_gpio.gpio = GPIO_PA1;
     switch_device[devi].relay_gpio.input = ON;
     switch_device[devi].relay_gpio.output = ON;;
